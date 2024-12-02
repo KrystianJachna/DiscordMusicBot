d8b85e20741ae562a696c76458283f917e8d1ca5
Krystian <krysti4nape@gmail.com>
2024-12-02 22:53:01 +0100

    Added load_token to load DiscordToken from .env file

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-
 from discord.ext import commands
 from dotenv import load_dotenv
 from os import getenv
 import asyncio
 
-# init
+
+def load_token() -> str:
+    """
+    Load the discord token from the .env file
+
+    :raises Exception: If the .env file does not exist
+    :raises Exception: If the token is not specified in the .env file
+    :return: The discord token
+    """
+    if not load_dotenv():
+        raise Exception(
+            "Failed to load .env file, please ensure it exists in the root directory of the project."
+        )
+    token = getenv("DISCORD_TOKEN")
+    if not token:
+        raise Exception(
+            "Failed to load token from .env file, please specify the token in the .env file. as DISCORD_TOKEN"
+        )
+    return token
 
 
 async def main():
-    TOKEN = getenv("DISCORD_TOKEN")
-    print(TOKEN)
+    TOKEN = load_token()
 
-asyncio.run(main())
+# asyncio.run(main())
