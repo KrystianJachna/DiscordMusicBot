f86f59007784687570d81571acb43b3252f4754f
Krystian Jachna <krysti4nape@gmail.com>
2024-12-10 03:25:16 +0100

    removed logging

--- cogs/music/music_downlaoder.py
+++ cogs/music/music_downlaoder.py
@@ -33,7 +33,6 @@ class Song:
 
         :return: FFmpegPCMAudio object.
         """
-        logging.info(self.stream_url)
         ffmpeg_options = {
             'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
             'options': '-vn'
