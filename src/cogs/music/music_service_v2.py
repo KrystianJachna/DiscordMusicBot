229f6997271a43403967cf511620bd2eff2b396b
Krystian Jachna <krysti4nape@gmail.com>
2024-12-29 00:16:34 +0100

    Refactor music queue handling and add new helper methods
    Refactored the music queue to improve structure and functionality. Introduced `TrackedAsyncQueue` for better queue management and added methods for retrieving queue info, length, and clearing the queue. Enhanced error handling during song downloading with appropriate feedback to users.

--- src/cogs/music/music_service_v2.py
+++ src/cogs/music/music_service_v2.py
@@ -7,9 +7,11 @@ from discord import VoiceClient
 from traceback import format_exc
 
 from .messages import *
-from .music_downlaoder import SongFactory
+from .music_downlaoder import SongDownloader
 from .music_service import MusicQueue
 from .song_cache import LRUSongsCache
+from discord.ext.commands import Context
+from .messages import *
 
 
 class SongQueue(ABC):
@@ -22,7 +24,7 @@ class SongQueue(ABC):
         pass
 
     @abstractmethod
-    async def add(self, query: str) -> None:
+    async def add(self, query: str, ctx: Context) -> None:
         pass
 
     @abstractmethod
@@ -31,6 +33,10 @@ class SongQueue(ABC):
 
     @abstractmethod
     async def get_queue_info(self) -> list[str]:
+        pass
+
+    @abstractmethod
+    async def queue_length(self) -> int:
         pass
 
 
@@ -78,8 +84,17 @@ class MusicPlayer:
         self._voice_client.stop()
         await self._voice_client.disconnect()
 
-    async def play(self, query: str) -> None:
-        await self._song_queue.add(query)
+    async def clear_queue(self) -> None:
+        await self._song_queue.clear_queue()
+
+    async def get_queue_info(self) -> list[str]:
+        return await self._song_queue.get_queue_info()
+
+    async def queue_length(self) -> int:
+        return await self._song_queue.queue_length()
+
+    async def play(self, query: str, ctx: Context) -> None:
+        await self._song_queue.add(query, ctx)
         if not self._processing_queue:
             self._processing_task = asyncio.create_task(self._process_song_queue())
 
@@ -93,7 +108,7 @@ class MusicPlayer:
                     if self._loop and self._looped_songs:
                         self._now_playing = self._looped_songs.pop(0)
                     else:
-                        break
+                        break  # TODO: disconnect after a timeout
                 source = await self._now_playing.get_source()
                 finished = asyncio.Event()
                 self._voice_client.play(source, after=lambda e: self._after_playing(e, finished))
@@ -114,24 +129,77 @@ class MusicPlayer:
 
 
 class BgDownloadSongQueue(SongQueue):
+    class TrackedAsyncQueue(asyncio.Queue):
+        def __init__(self):
+            super().__init__()
+            self._elements = []
 
-    def __init__(self, song_cache: LRUSongsCache):
-        self._queue: asyncio.Queue[str] = asyncio.Queue()
-        self._song_cache = song_cache
+        def put_nowait(self, item):
+            super().put_nowait(item)
+            self._elements.append(item)
+
+        def get_nowait(self):
+            item = super().get_nowait()
+            self._elements.remove(item)
+            return item
+
+        @property
+        def elements(self):
+            return self._elements
+
+    def __init__(self):
+        self._music_downloader = SongDownloader()
+        self._downloaded_songs: BgDownloadSongQueue.TrackedAsyncQueue = BgDownloadSongQueue.TrackedAsyncQueue()
+        self._waiting_queries: list[tuple[str, Context]] = []
+        self._now_processing: Optional[str] = None
+        self._processing_task: Optional[asyncio.Task] = None
+
 
     async def next(self) -> Song:
-        query = await self._queue.get()
-        song = self._song_cache.get(query)
-        if not song:
-            song = await SongFactory.from_query(query)
-            self._song_cache.add(song)
-        return song
+        if self._downloaded_songs.empty() and not self._processing_task:
+            raise SongQueue.EndOfPlaylistException
+        return await self._downloaded_songs.get()
 
-    async def add(self, query: str) -> None:
-        await self._queue.put(query)
+    async def add(self, query: str, ctx: Context) -> None:
+        self._waiting_queries.append((query, ctx))
+        if not self._processing_task:
+            self._processing_task = asyncio.create_task(self._process_queue())
 
     async def clear_queue(self) -> None:
-        self._queue = asyncio.Queue()
+        if self._processing_task:
+            self._processing_task.cancel()
+        self._waiting_queries.clear()
+        self._downloaded_songs = BgDownloadSongQueue.TrackedAsyncQueue()
 
     async def get_queue_info(self) -> list[str]:
-        return list(self._queue)
+        downloaded_songs = self._downloaded_songs.elements
+        now_processing = [self._now_processing] if self._now_processing else []
+        waiting_queries = [query for query, _ in self._waiting_queries]
+        return downloaded_songs + now_processing + waiting_queries
+
+    async def queue_length(self) -> int:
+        return len(await self.get_queue_info())
+
+    async def _process_queue(self) -> None:
+        try:
+            while self._waiting_queries:
+                query, ctx = self._waiting_queries.pop(0)
+                self._now_processing = query
+                try:
+                    song = await self._music_downloader.prepare_song(query)
+                    self._downloaded_songs.put_nowait(song)
+                except SongDownloader.AgeRestrictedException:
+                    await ctx.send(embed=age_restricted(query))
+                except SongDownloader.NoResultsFoundException:
+                    await ctx.send(embed=no_results(query))
+                except SongDownloader.LiveFoundException:
+                    await ctx.send(embed=live_stream(query))
+                except Exception as e:
+                    logging.error(e)
+                    logging.debug(format_exc())
+                    await ctx.send(embed=download_error(query))
+        except asyncio.CancelledError:
+            pass
+        finally:
+            self._now_processing = None
+            self._processing_task = None
