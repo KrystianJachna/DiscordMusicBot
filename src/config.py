2eec4b03d72c0cb095dfa89d6a7f8012db533754
Krystian Jachna <krysti4nape@gmail.com>
2024-12-29 22:04:04 +0100

    Add new configuration constants for file paths and caching
    Introduce `COOKIES_PATH` and `LOG_PATH` using `Path` for better file handling. Added `CACHE_SIZE` to manage application caching limits. These constants improve configurability and code clarity.

--- src/config.py
+++ src/config.py
@@ -1,3 +1,10 @@
+from pathlib import Path
+
 ERROR_COLOR = 0xFF5555
 SUCCESS_COLOR = 0x50C878
 INFO_COLOR = 0x89CFF0
+
+COOKIES_PATH = Path("cookies.txt")
+LOG_PATH = Path("bot.log")
+
+CACHE_SIZE = 100
